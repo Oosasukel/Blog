391c1a690c1ebd81f90f80ccf179df7ba7255173
Oosasukel <rodrigo_gonn@hotmail.com>
2021-08-09 20:02:21 -0300

    master

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            async
+            src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2341175168006454'
+            crossOrigin='anonymous'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
